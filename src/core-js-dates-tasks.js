7c57374d803d17aeded709f82327c834943194fa
EVISION\Otabek.Mirzakhmedov <otabek.mirzakhmedov@wolterskluwer.com>
2024-03-19 14:58:41 +0500

    conut days period

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -94,8 +94,15 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+  const millisecondsPerDay = 1000 * 60 * 60 * 24;
+  const differenceInMilliseconds = end - start;
+  const differenceInDays = Math.floor(
+    differenceInMilliseconds / millisecondsPerDay
+  );
+  return differenceInDays + 1;
 }
 
 /**
