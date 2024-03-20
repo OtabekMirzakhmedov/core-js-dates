aa5aabec1e115357462710dc85b2d50b86840f85
EVISION\Otabek.Mirzakhmedov <otabek.mirzakhmedov@wolterskluwer.com>
2024-03-20 10:18:50 +0500

    get date

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -49,8 +49,21 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const utcDate = new Date(date);
+  utcDate.setMinutes(utcDate.getMinutes() + utcDate.getTimezoneOffset());
+  const isoDate = utcDate.toISOString();
+  const dayIndex = new Date(isoDate).getDay();
+  return daysOfWeek[dayIndex];
 }
 
 /**
